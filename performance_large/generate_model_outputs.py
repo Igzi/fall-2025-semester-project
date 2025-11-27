7969ac8709b84196882ce0739fd68961c22f7866
Igzi <igor.pavlovic.2001@gmail.com>
2025-11-27 11:46:15 +0000

    Hf adapter outputs

--- performance_large/generate_model_outputs.py
+++ performance_large/generate_model_outputs.py
@@ -50,7 +50,6 @@ def load_peft_model(lora_module_list, base_model):
     """
     Load and configure PEFT (Parameter-Efficient Fine-Tuning) adapters onto the base model.
     """
-    device = "cuda" if torch.cuda.is_available() else "cpu"
     lora_lists = []
     for i, lora_model in enumerate(lora_module_list):
         if i == 0:
@@ -71,7 +70,7 @@ config_path = './config/config_large.json'
 data_path = './dataset/config_large_flat.json'
 res_path = './performance_large/outputs_13b/model_'
 results = []  # Initialize a list to store question and response data
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 # Initialize vector database for retrieval
 init_vector_db('./config/config_large.json')
@@ -117,7 +116,7 @@ peft_model = peft_model.to(device)
 peft_model.eval()
 
 with torch.no_grad():
-    for model_id in range(48):
+    for model_id in range(8,16):
         results = []
         with tqdm(total=len(dataset["train"]), desc="Evaluating", unit="item") as pbar:
             for i in range(0, len(eval_data["full_prompt"]), batch_size):
