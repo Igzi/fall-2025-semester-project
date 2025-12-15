b8f3a76c0fc01adb95e92d9e7708e8998287ce46
Igzi <igor.pavlovic.2001@gmail.com>
2025-12-15 14:16:56 +0000

    kmeans evaluation scripts

---
+++ scripts/bash/run_kmeans.bash
@@ -0,0 +1,9 @@
+python3 perf_eval_kmeans.py --data_path dataset/combined_test.json  --res_path kmeans_results/fusion_6_13b.json --eval_type mixture  --lora_num 3 --batch_size 1 --model_size 13b --device cuda:1 --num_clusters 6
+python3 perf_eval_kmeans.py --data_path dataset/combined_test.json  --res_path kmeans_results/fusion_12_13b.json --eval_type mixture  --lora_num 3 --batch_size 1 --model_size 13b --device cuda:1 --num_clusters 12
+python3 perf_eval_kmeans.py --data_path dataset/combined_test.json  --res_path kmeans_results/fusion_24_13b.json --eval_type mixture  --lora_num 3 --batch_size 1 --model_size 13b --device cuda:1 --num_clusters 24
+python3 perf_eval_kmeans.py --data_path dataset/combined_test.json  --res_path kmeans_results/fusion_48_13b.json --eval_type mixture  --lora_num 3 --batch_size 1 --model_size 13b --device cuda:1 --num_clusters 48
+python3 perf_eval_kmeans.py --data_path dataset/combined_test.json  --res_path kmeans_results/fusion_96_13b.json --eval_type mixture  --lora_num 3 --batch_size 1 --model_size 13b --device cuda:1 --num_clusters 96
+python3 perf_eval_kmeans.py --data_path dataset/combined_test.json  --res_path kmeans_results/fusion_192_13b.json --eval_type mixture  --lora_num 3 --batch_size 1 --model_size 13b --device cuda:1 --num_clusters 192
+python3 perf_eval_kmeans.py --data_path dataset/combined_test.json  --res_path kmeans_results/fusion_384_13b.json --eval_type mixture  --lora_num 3 --batch_size 1 --model_size 13b --device cuda:1 --num_clusters 384
+python3 perf_eval_kmeans.py --data_path dataset/combined_test.json  --res_path kmeans_results/fusion_768_13b.json --eval_type mixture  --lora_num 3 --batch_size 1 --model_size 13b --device cuda:1 --num_clusters 768
+python3 perf_eval_kmeans.py --data_path dataset/combined_test.json  --res_path kmeans_results/fusion_1536_13b.json --eval_type mixture  --lora_num 3 --batch_size 1 --model_size 13b --device cuda:1 --num_clusters 1536
